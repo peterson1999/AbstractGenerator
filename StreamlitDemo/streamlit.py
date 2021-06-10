d71ba19a4c9583880627edd337f04f8d3662577d
peterson1999 <51360811+peterson1999@users.noreply.github.com>
2021-06-10 12:08:43 +0800

    fefaf
    sdfasfa

--- StreamlitDemo/streamlit.py
+++ StreamlitDemo/streamlit.py
@@ -7,17 +7,17 @@ from summarizer import Summarizer
 # %%
 st.title("Abstract Generator")
 introduction = st.text_area('Introduction:') 
-ratio1 = st.slider('How much would you like to take from the introduction (%)?', 0, 100, 25)
-methodology = st.text_area('methodology:') 
-ratio2 = st.slider('How much would you like to take from the methodology (%)?', 0, 100, 25)
-results = st.text_area('results:')
-ratio3 = st.slider('How much would you like to take from the results (%)?', 0, 100, 25)
-conclusion = st.text_area('conclusion:') 
-ratio4 = st.slider('How much would you like to take from the conclusion (%)?', 0, 100, 25)
+ratio1 = st.slider('How much would you like to take from the Introduction (%)?', 0, 100, 25)
+methodology = st.text_area('Methodology:') 
+ratio2 = st.slider('How much would you like to take from the Methodology (%)?', 0, 100, 25)
+results = st.text_area('Results:')
+ratio3 = st.slider('How much would you like to take from the Results (%)?', 0, 100, 25)
+conclusion = st.text_area('Conclusion:') 
+ratio4 = st.slider('How much would you like to take from the Conclusion (%)?', 0, 100, 25)
 
 # %%
 if st.button('Generate'):
-    writer=st.write(Summarizer.getSummary(Summarizer,introduction,methodology,results,conclusion))
+    writer=st.write(Summarizer.getSummary(Summarizer,introduction,ratio1,methodology,ratio2,results,ratio3,conclusion,ratio4))
 
 
 # %%
--- StreamlitDemo/summarizer.py
+++ StreamlitDemo/summarizer.py
@@ -9,13 +9,13 @@ class Summarizer:
     torch_device =  'cuda' if torch.cuda.is_available() else 'cpu'
     model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
-    def textrank(introduction,methodology,results,conclusion):
+    def textrank(introduction,methodology,results,conclusion,ratio1,ratio2,ratio3,ratio4):
 
         splitter = SentenceSplitter(language='en')
-        sentence_introduction = splitter.split(summarizer.summarize(introduction,ratio=0.35))
-        sentence_methodology = splitter.split(summarizer.summarize(methodology,ratio=0.35))
-        sentence_results = splitter.split(summarizer.summarize(results,ratio=0.35))
-        sentence_conclusion = splitter.split(summarizer.summarize(conclusion,ratio=0.35))
+        sentence_introduction = splitter.split(summarizer.summarize(introduction,ratio=ratio1/100))
+        sentence_methodology = splitter.split(summarizer.summarize(methodology,ratio=ratio2/100))
+        sentence_results = splitter.split(summarizer.summarize(results,ratio=ratio3/100))
+        sentence_conclusion = splitter.split(summarizer.summarize(conclusion,ratio=ratio4/100))
         all_sentences = np.concatenate((np.array(sentence_introduction), np.array(sentence_methodology),np.array(sentence_results),np.array(sentence_conclusion)))
         print(all_sentences)
         return all_sentences
@@ -38,10 +38,12 @@ class Summarizer:
         return s
     def get_response(self,input_text,num_return_sequences):
         batch = self.tokenizer.prepare_seq2seq_batch([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(self.torch_device)
-        translated = self.model.generate(**batch,max_length=60,num_beams=10, num_return_sequences=num_return_sequences, temperature=1.5)
+        print("done")
+        translated = self.model.generate(**batch,min_length=10,max_length=60,num_beams=10, num_return_sequences=num_return_sequences, temperature=1.5)
+        print("done1")
         tgt_text = self.tokenizer.batch_decode(translated, skip_special_tokens=True)
         return tgt_text
-    def getSummary(self,introduction,methodology,results,conclusion):
-        sentence=self.textrank(introduction,methodology,results,conclusion)
+    def getSummary(self,introduction,ratio1,methodology,ratio2,results,ratio3,conclusion,ratio4):
+        sentence=self.textrank(introduction,methodology,results,conclusion,ratio1,ratio2,ratio3,ratio4)
         paraphrase=self.Paraphrase(self,sentence)
         return paraphrase
